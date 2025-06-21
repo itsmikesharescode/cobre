db7394609a109cdfea0fbe0113e2bf696992a6db
itsmikesharescode <eviotamikejohnb@gmail.com>
2025-06-21 11:48:34 +0800

    update siteheader

--- src/lib/components/externals/sidebar/site-header/site-header.svelte
+++ src/lib/components/externals/sidebar/site-header/site-header.svelte
@@ -2,6 +2,8 @@
 	import { Button } from '$lib/components/internals/button/index';
 	import { Separator } from '$lib/components/internals/separator/index';
 	import * as Sidebar from '$lib/components/internals/sidebar/index';
+	import DarkMode from '$lib/components/externals/dark-mode/dark-mode.svelte';
+	import ThemeSelector from '$lib/components/externals/theme-selector/theme-selector.svelte';
 </script>
 
 <header
@@ -10,18 +12,10 @@
 	<div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
 		<Sidebar.Trigger class="-ml-1" />
 		<Separator orientation="vertical" class="mx-2 data-[orientation=vertical]:h-4" />
-		<h1 class="text-base font-medium">Documents</h1>
+		<Button variant="outline" size="sm">Guides</Button>
 		<div class="ml-auto flex items-center gap-2">
-			<Button
-				href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
-				variant="ghost"
-				size="sm"
-				class="dark:text-foreground hidden sm:flex"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				GitHub
-			</Button>
+			<DarkMode />
+			<ThemeSelector />
 		</div>
 	</div>
 </header>
